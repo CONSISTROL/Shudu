bdefcee55e41cb79129fa1c35601605012ab3aaf
CONSISTROL <1472298635@qq.com>
2021-01-27 22:26:05 +0800

    编写check

--- 数独/数独.cpp
+++ 数独/数独.cpp
@@ -38,7 +38,17 @@ void output() {
 }
 
 bool check(int n, int key) {
-
+	int i, j;
+	int x = n / 9 / 3 * 3;//row
+	int y = n % 9 / 3 * 3;//column
+	for (j = 0; j < 8; j++)
+		if (num[n / 9][j] == key)return false;//row
+	for (i = 0; i < 8; i++)
+		if (num[i][n % 9] == key)return false;//column
+	for (i = x; i < x + 3; i++)
+		for (j = y; j < y + 3; j++)
+			if (num[i][j] == key)return false;//cell
+	return true;
 }
 
 void dfs(int n) {
