9f2f1c606bfb72baf261bdf7f2e556139aeea747
CONSISTROL <1472298635@qq.com>
2021-02-02 21:47:00 +0800

    修复多组输入错误的bug

--- 数独/数独.cpp
+++ 数独/数独.cpp
@@ -4,8 +4,10 @@
 using namespace std;
 
 int num[9][9];
-bool sign = false;
+bool sign;
 int program_time;
+int bar_time;
+int cnt;
 
 int input(int);
 void output(int);
@@ -18,10 +20,13 @@ int main() {
 		cout << "控制台输入请按1,文件输入请按2" << endl;
 		int n; cin >> n;
 		if (input(n)) return 0;
+		sign = false;
 		program_time = clock();//从这开始记录时间
+		bar_time = clock();
+		cnt = 0;
 		dfs(0);
 		output(1);
-		cout << "continue..?\n1.yes 2.no\n";
+		cout << "continue..?\n1.yes   2.no\n";
 		cin >> m;
 	} while (m==1);
 	return 0;
@@ -75,17 +80,8 @@ void output(int n) {
 }
 
 void bar(int n) {//进度条
-	static int cnt = 0;
-	static int bar_time = clock();
-	if (clock() - bar_time > 1000 && clock() - program_time > 4000) {//长时间未响应，给出提示
-		bar_time = clock();
-		//cnt = 0;//会回弹
-		printf(" waiting time:%2.0fs", float(bar_time - program_time) / 1000);
-		for (int i = 0; i < 17; i++)
-			printf("\b");
-	}
 	if (n > cnt) {//防回弹减速
-		printf("\r");//与system("\r")相比，防频闪，速度更快
+		printf("\r");//与system("cls")相比，防频闪，速度更快
 		int j;
 		for (j = 1; j <= n; j++) {
 			cout << "█";
@@ -96,6 +92,13 @@ void bar(int n) {//进度条
 		cout << "  " << n << "/81";
 		cnt++;
 		if (n == 81)cout << endl;
+	}
+	if (clock() - bar_time > 1000 && clock() - program_time > 4000) {//长时间未响应，给出提示
+		bar_time = clock();
+		//cnt = 0;//会回弹
+		printf(" waiting time:%2.0fs", float(bar_time - program_time) / 1000);
+		for (int i = 0; i < 17; i++)
+			printf("\b");
 	}
 }
 
